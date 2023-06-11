611bed62e0c5e99b17820458eb2a8d4cfc11bd82
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-06-11 22:44:24 +0300

    exercises 4.5 and 4.6

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,6 +7,35 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, curr) => sum + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const mostLiked = Math.max(...blogs.map(blog => blog.likes))
+  const favorite = blogs.find(blog => blog.likes === mostLiked)
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
+const mostBlogs = (blogs) => {
+  const authorArr = blogs.map(blog => blog.author)
+  const counts = {}
+  for (const item of authorArr) {
+    if (item in counts) {
+      counts[item]++
+    }else {
+      counts[item] = 1
+    }
+  }
+  const propKeys = Object.keys(counts)
+  const propValues = Object.values(counts)
+
+  return {
+    author: propKeys[propValues.indexOf(Math.max(...propValues))],
+    blogs: Math.max(...propValues)
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
