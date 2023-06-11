2749b346a97c1ac8aa9e312e0dd46e9366edf212
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-06-11 03:45:05 +0300

    exercise 4.2

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,41 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.post("/", (request, response, next) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.delete("/:id", (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
