18904a690b5138bf88fc9b0258937a06869e230e
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-07-24 16:20:55 +0300

    exercise 5.21

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -15,37 +15,35 @@ describe('Blog app', function() {
     cy.contains('password')
     cy.contains('login')
   })
-})
 
-describe('Login', function() {
-  it('succeeds with correct credentials', function() {
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.visit('http://localhost:3000')
+      cy.get('#username').type('testusername')
+      cy.get('#password').type('testpassword')
+      cy.get('#login-button').click()
+
+      cy.contains('Test User logged in')
+    })
+  })
+
+  it('fails with wrong credentials', function() {
     cy.visit('http://localhost:3000')
     cy.get('#username').type('testusername')
-    cy.get('#password').type('testpassword')
+    cy.get('#password').type('wrongpassword')
     cy.get('#login-button').click()
 
-    cy.contains('Test User logged in')
+    cy.contains('Wrong credentials')
   })
-})
 
-it('fails with wrong credentials', function() {
-  cy.visit('http://localhost:3000')
-  cy.get('#username').type('testusername')
-  cy.get('#password').type('wrongpassword')
-  cy.get('#login-button').click()
-
-  cy.contains('Wrong credentials')
-})
-
-describe('Blog App', function() {
   describe('When logged in', function() {
     beforeEach(function() {
       cy.visit('http://localhost:3000')
       cy.get('#username').type('testusername')
       cy.get('#password').type('testpassword')
       cy.get('#login-button').click()
-      cy.wait(1000)
     })
+
     it('A blog can be created', function() {
       cy.contains('New blog').click()
       cy.contains('Create new blog')
@@ -57,18 +55,35 @@ describe('Blog App', function() {
       cy.contains('Test Blog by Test Author')
     })
 
-    it('the user can like a blog', function() {
+    it('The user can like a blog', function() {
       cy.contains('New blog').click()
       cy.contains('Create new blog')
-      cy.get('#titleInput').type('Test Blog')
-      cy.get('#authorInput').type('Test Author')
-      cy.get('#urlInput').type('Test Url')
+      cy.get('#titleInput').type('Test Blog for Like')
+      cy.get('#authorInput').type('Test Author for Like')
+      cy.get('#urlInput').type('Test Url for Like')
       cy.get('#create-button').click()
 
-      cy.contains('Test Blog by Test Author')
+      cy.contains('Test Blog for Like by Test Author for Like')
       cy.contains('view').click()
+      cy.contains('0')
       cy.contains('like').click()
       cy.contains('1')
+    })
+
+    it('the user who created a blog can delete it', function() {
+      cy.contains('New blog').click()
+      cy.contains('Create new blog')
+      cy.get('#titleInput').type('Test Blog for Delete')
+      cy.get('#authorInput').type('Test Author for Delete')
+      cy.get('#urlInput').type('Test Url for Delete')
+      cy.get('#create-button').click()
+
+      cy.contains('Test Blog for Delete by Test Author for Delete')
+      cy.contains('view').click()
+      cy.contains('Remove').click()
+      cy.wait(6000)
+
+      cy.get('html').should('not.contain', 'Test Blog for Delete by Test Author for Delete')
     })
   })
 })
